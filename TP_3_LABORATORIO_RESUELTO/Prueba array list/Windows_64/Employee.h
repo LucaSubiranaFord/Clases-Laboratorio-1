f216fb038309c9e9ca33e32f5986e9cc6a9fc7e8
Luca Subirana <lucasubiranaford@gmail.com>
2017-06-02 09:26:03 -0300

    Add files via upload

---
+++ TP_3_LABORATORIO_RESUELTO/Prueba array list/Windows_64/Employee.h
@@ -0,0 +1,24 @@
+#ifndef _EMPLOYEE_H
+#define _EMPLOYEE_H
+
+typedef struct{
+    char titulo[20];
+    char genero[20];
+    int duracion;
+    char descripcion[50];
+    int puntaje;
+    char linkImagen[50];
+}eMovie;
+
+
+agregarPelicula(ArrayList*);
+borrarPelicula(ArrayList*);
+eMovie* newMovie();
+modificarPelicula(ArrayList*);
+generarPagina(ArrayList*);
+
+
+#endif // _EMPLOYEE_H
+
+
+
---
+++ TP_3_LABORATORIO_RESUELTO/Prueba array list/Windows_64/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+    ArrayList* lista;
+    eMovie* aux;
+    lista = al_newArrayList();
+    char nombre[10] = "Luca";
+    int edad = 15;
+    char nombre2[10] = "Gonza";
+    int edad2 = 17;
+    int i;
+
+    while(seguir=='s')
+    {
+        printf("1- Agregar pelicula\n");
+        printf("2- Borrar pelicula\n");
+        printf("3- Modificar pelicula\n");
+        printf("4- Generar pagina web\n");
+        printf("5- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                agregarPelicula(lista);
+                break;
+            case 2:
+                borrarPelicula(lista);
+                break;
+            case 3:
+                modificarPelicula(lista);
+               break;
+               case 4:
+                   generarPagina(lista);
+               break;
+            case 5:
+                 for(i=0;i<al_len(lista);i++)
+                {
+                    aux = newMovie();
+                    aux = al_get(lista,i);
+                    printf("%s --- %d\n", aux->titulo, aux->puntaje);
+                }
+                seguir = 'n';
+                break;
+        }
+    }
+
+    return 0;
+}
